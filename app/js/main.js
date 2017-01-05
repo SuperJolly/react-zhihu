d9ab1db768d60e2c03ad4825268665a3f0fdee42
jeremygao <jeremygao@tencent.com>
2017-01-05 22:23:44 +0800

    add babel

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,20 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var QuestionApp = require('./containers/QuestionApp');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import QuestionApp from './containers/QuestionApp';
 
-var mainCom = ReactDOM.render(
-	<QuestionApp />,
-	document.getElementById('app')
-);
+// var React = require('react');
+// var ReactDOM = require('react-dom');
+// var QuestionApp = require('./containers/QuestionApp');
+
+export default class mainCom extends ReactDOM {
+	
+}
+
+
+
+
+
+// var mainCom = ReactDOM.render(
+// 	<QuestionApp />,
+// 	document.getElementById('app')
+// );
